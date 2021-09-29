0defd44753c9d6b729709a036f0dab91a990a3f3
Devender00999 <Devender00999@gmail.com>
2021-09-29 20:57:43 +0530

    Added MainPage and removed few folders

--- 7. jQuery/index.js
+++
@@ -1,7 +0,0 @@
-$("h1").on("mouseover", function (event) {
-  $("h1").css("color", "blue").css("cursor", "pointer");
-});
-
-$("h1").on("mouseout", function (event) {
-  $("h1").css("color", "black");
-});
