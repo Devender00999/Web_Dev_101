40ecd7fe9c5ee04979fdea1649749a999903cff0
Devender <Devender00999@gmail.com>
2021-10-07 06:43:52 +0530

    Added WeatherProject

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,16 +1,46 @@
 const express = require("express"); // importing express
 const https = require("https"); // importing https
 const app = express(); // creating express app
-app.use(express.json());
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // creating home route
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const units = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&units=" +
+    units +
+    "&appid=" +
     process.env.API_KEY;
   https.get(url, (response) => {
-    console.log(response);
-    res.send("Hello World!");
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const icon =
+        "http://openweathermap.org/img/wn/" +
+        weatherData.weather[0].icon +
+        "@2x.png";
+      console.log(icon);
+      res.write("<p>The Weather is currently " + desc + "</p>");
+
+      res.write(
+        "<h1>The temperature in " +
+          query +
+          " is " +
+          temp +
+          " degree Celcius. </h1>"
+      );
+      res.write("<img src=" + icon + " />");
+      res.send();
+    });
   });
 });
 
